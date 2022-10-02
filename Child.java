d965a8166458ac057a28aed939e2f98de02348ee
Neha Rani <114100293+neharani1234@users.noreply.github.com>
2022-10-02 10:04:54 +0530

    Add files via upload

---
+++ Child.java
@@ -0,0 +1,23 @@
+package com.java;
+
+// Here we Create child class which inherit property from its parent.
+
+      class Child extends Parent 
+{
+	
+	// This is a Speak method .
+	
+     void Speak()
+ {
+System.out.println("I am Speaking Fluent english.")	; 
+ }
+ 
+ // This is dance method.
+ 
+          void Dance()
+ {
+ System.out.println("Hey,I am dancing very Well.");
+	 
+ }
+ 
+}
---
+++ Parent.java
@@ -0,0 +1,23 @@
+package com.java;
+
+// Here we creat parent class.
+   
+class Parent {
+	      void parent() 
+	{
+		
+		//This is a constructor.
+		
+		System.out.println("Hey I am Your parent.");
+		
+	}
+	
+	//And This is a Sing method.
+	
+        void Sing() 
+    {
+    	System.out.println("I am Singing.");
+    }
+    
+
+}
