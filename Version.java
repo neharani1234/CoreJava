1d74a80c4e6e63eef201e1a8be26f92078396cb1
Neha Rani <114100293+neharani1234@users.noreply.github.com>
2022-10-02 10:08:00 +0530

    Add files via upload

---
+++ Version.java
@@ -0,0 +1,24 @@
+package com.java;
+
+/* Program:-Find the version of Java.
+ * 
+ * @author:-Neha Rani
+ * @date:-01-10-2022
+ */
+
+
+//Declaration of Main  class
+
+ class Version 
+ {
+	 
+  public static void main(String[] args)
+  
+  //Here we Print of java version.
+  
+    {
+         System.out.println("version of Java is: "+ System.getProperty("java.version"));	
+    }   
+	
+
+}
