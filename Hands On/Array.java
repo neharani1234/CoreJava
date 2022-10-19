f377d6325e2c8e7efab2cdafb550ed3eb1852622
Neha Rani <114100293+neharani1234@users.noreply.github.com>
2022-10-19 15:31:33 +0530

    Add files via upload

---
+++ Hands On/Array.java
@@ -0,0 +1,14 @@
+package com.java;
+
+ class Array {
+	 public static void main(String[] args)
+	 {
+		 String name[]= {"Ram","Shyam","Sita","Gita"};
+		 
+		 for(int i=0;i<name.length;i++)
+		 {
+			System.out.println(name[i]);
+		 }
+	 }
+
+}
